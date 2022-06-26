ab04f05075954eada6ff70e6b818eb83da909053
Veraemail <vgdovskaya@srk.ru.com>
2022-06-26 22:05:40 +0300

    готово

--- Задача_2/Program.cs
+++ Задача_2/Program.cs
@@ -9,9 +9,11 @@ int number_1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("input second number: ");
 int number_2 = Convert.ToInt32(Console.ReadLine());
 
+int max = number_1;
+
 if ( number_1 > number_2 )   
-    Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> a ");
+    Console.WriteLine("a = " + number_1 + ", b = " + number_2 + " -> max = " + number_1 + "");
 else
-    Console.WriteLine("a = " + number_1 + ", b = " + number_2 +" -> b ");
+    Console.WriteLine("a = " + number_1 + ", b = " + number_2 +" -> max = " + number_2 + "");
 
  
