b6804f5b487febe086ff68b211092024e8467972
Veraemail <vgdovskaya@srk.ru.com>
2022-06-30 15:09:09 +0300

    чуть подправила

--- Задача_13/Program.cs
+++ Задача_13/Program.cs
@@ -6,15 +6,20 @@
 void randomThirdnumber(){
 int number = new Random().Next(0, 99999);
 Console.Write(number + " ->");
-if (number < 1000) {
+if (number < 100) {
     Console.WriteLine("no third number");
+}
+if (number > 99 & number < 1000)
+{
+    Console.WriteLine(number % 100 % 10);
 }
 if (number > 999 & number < 10000)
 {
-    Console.WriteLine(number % 1000 % 100 / 10);
+    Console.WriteLine(number % 1000 % 100 / 10);  
 }
-else {
-    Console.WriteLine(number % 10000 % 1000 / 100);  
+if (number > 9999 & number < 100000)
+{
+   Console.WriteLine(number % 10000 % 1000 / 100);  
 }
 }
 
