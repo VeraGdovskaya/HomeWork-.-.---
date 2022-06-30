af082bf8630e3dcca77cb169431a157bc9d0898b
Veraemail <vgdovskaya@srk.ru.com>
2022-06-30 13:55:28 +0300

    week

---
+++ Задача_15/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+void randomWeek(){
+int number = new Random().Next(1, 7);
+Console.Write(number + "->");
+if (number > 6){
+    Console.WriteLine("yes");
+}
+else {
+    Console.WriteLine("no");
+}
+}
+
+randomWeek();
