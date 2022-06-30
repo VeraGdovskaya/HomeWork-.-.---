4cb1d5244cd442a3d681188626710013b2371f0b
Veraemail <vgdovskaya@srk.ru.com>
2022-06-30 14:10:09 +0300

    добавила вызов метода 5 раз

--- Задача_13/Program.cs
+++ Задача_13/Program.cs
@@ -1,6 +1,10 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-int number = new Random().Next(0, 100000);
+
+
+
+void randomThirdnumber(){
+int number = new Random().Next(0, 99999);
 Console.Write(number + " ->");
 if (number < 1000) {
     Console.WriteLine("no third number");
@@ -12,4 +16,11 @@ if (number > 999 & number < 10000)
 else {
     Console.WriteLine(number % 10000 % 1000 / 100);  
 }
+}
+
+for (int i = 0; i <5; i++)
+{
+    randomThirdnumber();
+}
+
 
