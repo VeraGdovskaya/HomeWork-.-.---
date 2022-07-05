6e774c98c6e2700695ea264815011a67b2d658c1
Veraemail <vgdovskaya@srk.ru.com>
2022-07-05 22:05:09 +0300

    Задача2

--- 04.07.22/Program.cs
+++ 04.07.22/Program.cs
@@ -2,18 +2,31 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-Console.Write("Imput number A: ");
-int A = Convert.ToInt32(Console.ReadLine());
-Console.Write("Imput number B: ");
-int B = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Imput number A: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Imput number B: ");
+// int B = Convert.ToInt32(Console.ReadLine());
 
-int C = A;
+// int C = A;
 
-for (int i = 1; i < B; i++)
-{   
-     C = C * A;
+// for (int i = 1; i < B; i++)
+// {   
+//      C = C * A;
+// }
+
+// Console.WriteLine($" {A} , {B}  -> {C}");
+
+
+Console.Write("Imput number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write(number + " -> ");
+
+int sum = 0;
+
+while ( number > 0 )
+{
+  int k = number % 10;
+  number = number / 10;
+  sum = sum + k;
 }
-
-Console.WriteLine($" {A} , {B}  -> {C}");
-
-
+Console.WriteLine(sum);
