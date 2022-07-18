67952d721225b2a36145b672f4dc005f1619be1c
Veraemail <vgdovskaya@srk.ru.com>
2022-07-18 18:02:25 +0300

    очень тяжело дались задачки

---
+++ 14.07.22/Program.cs
@@ -0,0 +1,155 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+// double[,] GetArray(int m, int n)
+// {
+//     double[,] result = new double[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().NextDouble()*20 - 10; 
+//             result[i, j] = Math.Round(result[i, j], 1);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArray(double[,] inArray)
+// {
+//     for (int row = 0; row < inArray.GetLength(0); row++)
+//     {
+//         for (int column = 0; column < inArray.GetLength(1); column++)
+//         {
+//             Console.Write($"{inArray[row,column]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// Console.Write("Введите количество строк массива: ");
+// int rows=Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите количество столбцов массива: ");
+// int columns=Convert.ToInt32(Console.ReadLine());
+
+// double[,] array = GetArray(rows, columns);
+// PrintArray(array);
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//  и возвращает значение N-го элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+// int[,] GetArray(int m, int n, int minValue, int maxValue)
+// {
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(minValue, maxValue + 1);
+//         }
+//     }
+//     return result;
+// }
+
+
+
+// void PrintArray(int[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{inArray[i,j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// Console.Write("Введите количество строк массива: ");
+// int rows=Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int columns=Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите номер строки элемента, который нужно найти: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите номер столбца элемента, который нужно найти: ");
+// int d = Convert.ToInt32(Console.ReadLine());
+// int[,] array = GetArray(rows, columns, 0, 100);
+// PrintArray(array);
+// Console.Write($"{b}{d}");
+
+// if (b < array.GetLength(0) && d < array.GetLength(1)){
+//    Console.Write (" -> такое число есть ");   
+// }
+// else{
+//     Console.Write (" -> такго числа нет ");   
+// }
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue) // создает двумерный массив
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+    
+}
+
+
+
+void PrintArray(int[,] inArray) // вывводит двумерный массив
+{
+    for (int row = 0; row < inArray.GetLength(0); row++)
+    {
+        for (int column = 0; column < inArray.GetLength(1); column++)
+        {
+            Console.Write($"{inArray[row,column]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns=Convert.ToInt32(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+Console.Write("среднее арифметическое каждого столбца:" );
+   
+for (int j = 0; j < array.GetLength(1); j++)
+{
+double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum = sum + array[i,j];
+        }
+        
+        Console.Write($"{sum/rows} ");
+} 
+
